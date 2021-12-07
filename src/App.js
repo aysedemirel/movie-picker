37d2e33d1b902de50e8ec1fbbdf1fc1801065d52
aysedemirel <aysedemirel1996@gmail.com>
2021-12-07 23:11:18 +0300

    [NEW] Add lamp switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+// Components
+import Lamp from './Lamp';
+import LightSwitch from './LightSwitch';
 
-const App = () => {
+const Room = styled.div`
+  position: relative;
+  width: 500px;
+  height: 500px;
+  border: 10px solid black;
+  margin: 0 auto;
+`;
+
+function App() {
+  // Lamp one
+  const [isLampOneOn, setIsLampOneOn] = useState(false);
+  // Lamp two
+  const [isLampTwoOn, setIsLampTwoOn] = useState(true);
+
+  const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
+  const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
+
   return (
-    <div className="App">
-      Start here.
-    </div>
+    <Room>
+      <Lamp lampOn={isLampOneOn} position='left' />
+      <Lamp lampOn={isLampTwoOn} position='right' />
+      <LightSwitch
+        name='one'
+        callback={handleLightSwitchOne}
+        switchOn={isLampOneOn}
+        position='left'
+      />
+      <LightSwitch
+        name='two'
+        callback={handleLightSwitchTwo}
+        switchOn={isLampTwoOn}
+        position='right'
+      />
+    </Room>
   );
 }
 
