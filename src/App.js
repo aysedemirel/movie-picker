8b924c2735e97ffb88d794fc076ef040c23aac03
aysedemirel <aysedemirel1996@gmail.com>
2021-12-10 23:19:30 +0300

    [NEW] Add header component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-// Components
-import Lamp from './Lamp';
-import LightSwitch from './LightSwitch';
-
-const Room = styled.div`
-  position: relative;
-  width: 500px;
-  height: 500px;
-  border: 10px solid black;
-  margin: 0 auto;
-`;
+import React from 'react';
+import Header from './components/Header';
+// Styles
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
-  // Lamp one
-  const [isLampOneOn, setIsLampOneOn] = useState(false);
-  // Lamp two
-  const [isLampTwoOn, setIsLampTwoOn] = useState(true);
-
-  const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
-  const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
-
   return (
-    <Room>
-      <Lamp lampOn={isLampOneOn} position='left' />
-      <Lamp lampOn={isLampTwoOn} position='right' />
-      <LightSwitch
-        name='one'
-        callback={handleLightSwitchOne}
-        switchOn={isLampOneOn}
-        position='left'
-      />
-      <LightSwitch
-        name='two'
-        callback={handleLightSwitchTwo}
-        switchOn={isLampTwoOn}
-        position='right'
-      />
-    </Room>
+    <div>
+      <Header />
+      Start Here
+      <GlobalStyle />
+    </div>
   );
 }
 
