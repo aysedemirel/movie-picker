27846c15e7bbf89d38b2ac74b7b1fe76814ecf88
aysedemirel <aysedemirel1996@gmail.com>
2021-12-14 21:36:15 +0300

    [REFC] Style header component

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,9 +1,33 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    background: var(--darkGrey);
+    padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: var(--maxWidth);
+    padding: 0 20px;
+    margin: 0 auto;
+`;
 
-export const LogoImg =styled.img``;
+export const LogoImg = styled.img`
+    width: 200px;
 
-export const TMDBLogoImg = styled.img``;
+    @media screen and (max-width: 500px)
+    {
+        width: 150px;
+    }
+`;
+
+export const TMDBLogoImg = styled.img`
+    width: 100px;
+
+    @media screen and (max-width: 500px)
+    {
+        width: 50px;
+    }
+`;
