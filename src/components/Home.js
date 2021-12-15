a9cde3a43328fa10db01e559b246c20728da5fd8
aysedemirel <aysedemirel1996@gmail.com>
2021-12-15 22:32:29 +0300

    [NEW] Add thumb

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 //Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
@@ -26,9 +27,17 @@ const Home = () => {
             }
             <Grid header='Popular Movies'>
                 {state.results.map(movie => (
-                    <div key={movie.id}>{movie.title}</div>
+                    <Thumb
+                        key={movie.div}
+                        clickable={true}
+                        image={
+                            movie.poster_path
+                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
                 ))}
-
             </Grid>
         </div>
     );
