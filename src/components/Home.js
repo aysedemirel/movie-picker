1b4a4e1433b40d2ad4550a674f0718f0fd0fed45
aysedemirel <aysedemirel1996@gmail.com>
2021-12-15 22:38:45 +0300

    [NEW] Add spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
@@ -39,6 +40,7 @@ const Home = () => {
                     />
                 ))}
             </Grid>
+            <Spinner/>
         </div>
     );
 }
