3ca65b5a74c689ec521dbfa55e638b260da71c36
aysedemirel <aysedemirel1996@gmail.com>
2021-12-15 22:20:53 +0300

    [NEW] Add grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 //Components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
@@ -16,13 +17,19 @@ const Home = () => {
         <div>
             {state.results[0] ? (
                 <HeroImage
-                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                title={state.results[0].original_title}
-                text={state.results[0].overview}
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
                 />
-                )
+            )
                 : null
             }
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+
+            </Grid>
         </div>
     );
 }
