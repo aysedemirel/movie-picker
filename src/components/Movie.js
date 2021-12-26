2cbc2a1ddb2fd3bbee6c81265c7779bf4e163463
aysedemirel <aysedemirel1996@gmail.com>
2021-12-26 14:39:39 +0300

    [NEW] Add movie hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+// Image
+import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>;
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    console.log(movie);
+    return (
+        <>
+            <div>Movie</div>
+        </>
+    );
+};
 
 export default Movie;
